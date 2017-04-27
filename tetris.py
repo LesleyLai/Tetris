24b736d2c21bb943d1f9e385e4daa07a92a3b96b
Lesley Lai <laisililai@live.cn>
2017-04-27 10:37:52 -0600

    More gui setting

--- tetris.py
+++ tetris.py
@@ -3,8 +3,9 @@ The main file for the Tetris game
 """
 
 from tetris_gui import *
+import tkinter as tk
 
-class Tetris(tk.Frame):
+class Tetris:
     """
     The main Tetris class
     """
@@ -15,39 +16,43 @@ class Tetris(tk.Frame):
         """
         self.root = tk.Tk()
         self.root.title("Tetris")
-        super().__init__(self.root)
 
-        self.pack()
-
-        self.canvas = tk.Canvas(self, bg="green", width=240, height=600)
-        self.canvas.pack(side="bottom")
-
+        self.score = 0
         self._create_buttons()
+
+        self.status_var = tk.StringVar() 
+        self.status_var.set("Score: 0, Level: 1")
+        self.status = tk.Label(self.root,
+                               textvariable=self.status_var, 
+                               font=("Helvetica", 10, "bold"))
+        self.status.pack()
+        
+        self.canvas = tk.Canvas(
+                self.root, width=300, height=500, bg="green")
+        self.canvas.pack()
+
+        self.root.mainloop()
 
     def _create_buttons(self):
         """
         Creates buttons for the tetris game
-        """
-        self.buttons = tk.Frame(self)
-        self.buttons.pack(side="top")
-
-        self.new_bottom = TetrisButtom(self.buttons, "new game", \
-                                       "lightcoral")
-        #self.new_buttom["command"] = self.new_game
+        """        
+        color = "lightcoral"
+        self._buttoms = tk.Frame(self.root)
+        self._buttoms.pack()
+        
+        self.new_bottom = TetrisButtom(self._buttoms, "new game", color)
         self.new_bottom.pack(side="left")
 
-        self.pause_bottom = TetrisButtom(self.buttons, "pause", \
-                                       "lightcoral")
+        self.pause_bottom = TetrisButtom(self._buttoms, "pause", color)
         self.pause_bottom.pack(side="left")
 
-        self.quit_bottom = TetrisButtom(self.buttons, "quit", \
-                                        "lightcoral", \
+        self.quit_bottom = TetrisButtom(self._buttoms, "quit", color,
                                         self.root.destroy)
         self.quit_bottom.pack(side="left")
-
+        
 def main():
-    app = Tetris()
-    app.mainloop()
+    game = Tetris()
 
 if __name__ == '__main__':
     main()
