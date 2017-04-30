3121c2494a8f1c6db9e6ef34fb640545d4d13379
Lesley Lai <laisililai@live.cn>
2017-04-29 19:57:17 -0600

    The main dropping loop of the Tetris

--- tetris.py
+++ tetris.py
@@ -21,6 +21,18 @@ class TetrisPiece:
         self.moved = True
         self.position = (4, 1)
 
+    def drop_one(self):
+        """
+        Drop the piece by one
+        """
+        self.move((0, 1), 0)
+
+    def move(self, delta_position, rotation):
+        self.moved = True
+        if self.moved:
+            self.position = (self.position[0] + delta_position[0],
+                             self.position[1] + delta_position[1])
+
     @classmethod
     def generate_piece(cls, board):
         return TetrisPiece(random.sample(cls.ALL_PIECES, 1)[0], board)
@@ -29,8 +41,8 @@ class TetrisPiece:
                   [[(0, 0), (-1, 0), (1, 0), (2, 0)], # long
                    [(0, 0), (0, -1), (0, 1), (0, 2)]]]
         
-    ALL_COLORS = ['DarkGreen', 'dark blue', 'blue', 'dark red', 'gold2',
-                    'Purple3', 'OrangeRed2', 'LightSkyBlue']
+    ALL_COLORS = ['DarkGreen', 'dark blue', 'blue', 'dark red',
+                  'gold2', 'Purple3', 'OrangeRed2', 'LightSkyBlue']
         
 
 class TetrisBoard:
@@ -45,6 +57,11 @@ class TetrisBoard:
         self.block_size = 18
         self.columns_count = 10
         self.rows_count = 27
+
+        # Grid of the blocks store the information whether a block
+        # occupies a point
+        self.grid = [[False for x in range(self.rows_count)]
+                     for y in range(self.columns_count)]
         
         self.score = 0
         self.level = 1
@@ -62,6 +79,7 @@ class Tetris:
         self.root = tk.Tk()
         self.root.title("Tetris")
 
+        self.paused = False
         self.score = 0
         self._create_buttons()
 
@@ -69,14 +87,18 @@ class Tetris:
         self._create_canvas()
         self._create_bindings()
 
-        self.draw_piece(self.board.current_block)
+         # Dropping interval of the current piece (ms)
+        self.interval = 500
 
+        self.shape = self.draw_piece(self.board.current_block)
+
+        self.update()
         self.root.mainloop()
 
     def draw_piece (self, piece, old=None):
         """
-        takes a piece and optionally the list of old TetrisRects
-        corresponding to it and returns a new set of TetrisRects 
+        takes a piece and optionally the list of old rectangle
+        corresponding to it and returns a new set of rectangle
         which are how the piece is visible to the user.
         """
         block_size = self.board.block_size
@@ -95,9 +117,38 @@ class Tetris:
                                            block_size + x,
                                            block_size + y,
                                            fill=color)
+            new_shape.append(rect)
+        return new_shape
 
-        
-        
+    def update(self):
+        """
+        Makes the current_block drop by one, do nothing if the game 
+        paused.
+        """
+        if not self.paused:
+            self.board.current_block.drop_one()
+            self.shape = self.draw_piece(self.board.current_block,
+                                         self.shape)
+            
+        self.root.after(self.interval, self.update)
+
+    def pause_game(self):
+        """
+        Pauses the game
+        """
+        self.paused = True
+        self._pause_bottom["text"] = "continue"
+        self._pause_bottom['bg'] = "green"
+        self._pause_bottom['command'] = self.continue_game
+
+    def continue_game(self):
+        """
+        Continues the game after pause
+        """
+        self.paused = False
+        self._pause_bottom["text"] = "pause"
+        self._pause_bottom['bg'] = "lightcoral"
+        self._pause_bottom['command'] = self.pause_game
 
     def _create_buttons(self):
         """
@@ -110,7 +161,8 @@ class Tetris:
         self._new_bottom = TetrisButtom(self._buttoms, "new game", color)
         self._new_bottom.pack(side="left")
 
-        self._pause_bottom = TetrisButtom(self._buttoms, "pause", color)
+        self._pause_bottom = TetrisButtom(self._buttoms, "pause",
+                                          color, self.pause_game)
         self._pause_bottom.pack(side="left")
 
         self._quit_bottom = TetrisButtom(self._buttoms, "quit", color,
@@ -128,7 +180,8 @@ class Tetris:
         self.canvas = tk.Canvas(
             self.root,
             width=self.board.block_size * self.board.columns_count,
-            height=self.board.block_size * self.board.rows_count)
+            height=self.board.block_size * self.board.rows_count,
+            bg="purple")
         self.canvas.pack()
 
     def _create_bindings(self):
